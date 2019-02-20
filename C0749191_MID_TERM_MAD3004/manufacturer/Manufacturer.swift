e94841e0ecf6688cc34e63c4286556e074f7e183
MacStudent <macstudent@126-iMac15.local>
2019-02-20 18:50:32 -0500

    created files for manufacturer

---
+++ C0749191_MID_TERM_MAD3004/manufacturer/Manufacturer.swift
@@ -0,0 +1,28 @@
+//
+//  Manufacturer.swift
+//  C0749191_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-02-20.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Manufacturer:IDisplay
+{
+    
+    var mfgid:Int?
+    var mfgname:String?
+    
+    init() {
+        self.mfgid=Int()
+        self.mfgname=String()
+        <#statements#>
+    }
+    
+    func displaydata(msg: Bool, no: Int) {
+        <#code#>
+    }
+    
+    
+}
