0d8d736e107c57a158cd9b98e35e69ce11472700
MacStudent <macstudent@126-iMac15.local>
2019-02-20 18:42:30 -0500

    created new project and  added groups

--- C0749191_MID_TERM_MAD3004/main.swift
+++ C0749191_MID_TERM_MAD3004/main.swift
@@ -8,5 +8,29 @@
 
 import Foundation
 
-print("Hello, World!")
+
+
+
+
+var n:Int?
+
+var check:Bool=false
+repeat{
+    print("Please select an option: \n1 - ADD \n2 - DISPLAY \n3 - Display \n 4.Exit")
+    n = Int(readLine()!)
+    
+    switch n{
+    case 1:
+        break
+    case 2:
+        break
+    case 3:
+       break
+    default:
+        break
+    }
+    
+    
+}
+    while(n! <= 4)
 
